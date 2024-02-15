f6611638c0b0b935929fcad8168906018b9c12ef
LecasB <cleonardo191@gmail.com>
2024-02-15 01:06:01 +0000

    Segundo Commit - Formataçao Tab

--- battalhaNaval.swift
+++ battalhaNaval.swift
@@ -1,8 +1,28 @@
 var tabVis: [[String]] = Array(repeating: Array(repeating: "--", count: 11), count: 11)
 var tabInvis: [[String]] = Array(repeating: Array(repeating: "--", count: 11), count: 11)
 
+// Print the first horizontal line with numbers
+print("   ", terminator: "")
+for i in 0...11 {
+    if (i==0){
+        print(" 🚢 ",terminator: "")
+    }
+    else if (i == 10 || i == 11){
+    print("\(i) ", terminator: "")
+    }else{
+    print("0\(i) ", terminator: "")
+    }
+}
 
-for row in tabVis {
+print()
+
+// Print the remaining rows
+let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+for (index, row) in tabVis.enumerated() {
+    // Print the first vertical line with letters
+    print("\(letters[index])  ", terminator: "")
+    
+    // Print the elements of the row
     for element in row {
         print("\(element) ", terminator: "")
     }
